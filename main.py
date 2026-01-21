4bc907831a1d657e9adc77e0a7a03e7bfacc1f33
groovey <christian.grove.knutsen@gmail.com>
2026-01-21 14:23:20 +0100

    chapter 3: player triangle added

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ import pygame
 from constants import SCREEN_WIDTH
 from constants import SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    test_player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
 
     while True:
@@ -16,6 +18,7 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        test_player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 if __name__ == "__main__":
